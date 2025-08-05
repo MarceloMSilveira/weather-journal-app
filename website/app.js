61a5087faaf63a4d80b12a17f3c953d7adbc613e
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-05 14:31:34 -0300

    set styles

--- website/app.js
+++ website/app.js
@@ -3,3 +3,13 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+function setCurrentYearInFooter() {
+  const spanElement = document.querySelector("#this-year");
+  const thisYear = new Date().getFullYear();
+  console.log('test');
+  console.log(thisYear);
+  spanElement.textContent = thisYear;
+}
+
+setCurrentYearInFooter();
