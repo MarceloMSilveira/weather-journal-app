bccab506262b23b4fb7cab79dec99d32df491abb
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-08-06 17:10:19 -0300

    server started

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,42 @@
+
+import path from "path";
+const __dirname = import.meta.dirname;
+
+
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {
+  date: 'test',
+  place: 'test',
+  temperature: 'test',
+  user_feeling: 'test'
+};
 
 // Require Express to run server and routes
+import express from "express";
+import cors from "cors";
 
 // Start up an instance of app
 
+const app = express();
+const port = 3010;
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use(express.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static(path.resolve(__dirname,'../dist')));
+
+app.get('/test',(req,res)=>{
+  res.send('Success!');
+})
 
 
 // Setup Server
+app.listen(port, ()=>{
+  console.log(`server up on port ${port}`);
+})
--- backend/test.js
+++ backend/test.js
@@ -2,6 +2,6 @@ import path from 'node:path';
 
 const __dirname = import.meta.dirname;
 
-console.log(__dirname);
+//console.log(__dirname);
 
 console.log(path.resolve(__dirname,'../'));
